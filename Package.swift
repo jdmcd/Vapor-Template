92f97d23bcd1477c1fd086b56de12113d4d783cd
Jimmy School <mcdappdev@gmail.com>
2016-12-12 20:55:49 -0600

    update to new vapor version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Template",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 2),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
         .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1)
     ],
     exclude: [
