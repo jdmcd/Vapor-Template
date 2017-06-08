7591175fc3e5dad82cceda019c05e4a5d61e3405
Jimmy McDermott <mcdappdev@gmail.com>
2017-06-08 13:47:54 -0500

    add `frontend` method

--- Sources/App/Utils/VersionRoute.swift
+++ Sources/App/Utils/VersionRoute.swift
@@ -1,4 +1,5 @@
 import Routing
+import Flash
 
 struct VersionRoute {
     static let path = "api/v1"
@@ -12,4 +13,31 @@ extension RouteBuilder {
     func versioned() -> RouteBuilder {
         return grouped(VersionRoute.path)
     }
+}
+
+extension RouteBuilder {
+    fileprivate func middleware(_ type: FrontendMiddlewareType) -> [Middleware] {
+        var middleware: [Middleware] = [
+            FlashMiddleware()
+        ]
+        
+        if type == .all {
+            middleware.append(AuthedMiddleware())
+        }
+        
+        return middleware
+    }
+    
+    func frontend(_ type: FrontendMiddlewareType = .all, handler: (RouteBuilder) -> ()) {
+        group(middleware: middleware(type), handler: handler)
+    }
+    
+    func frontend(_ type: FrontendMiddlewareType = .all) -> RouteBuilder {
+        return grouped(middleware(type))
+    }
+}
+
+enum FrontendMiddlewareType {
+    case all
+    case noAuthed
 }
